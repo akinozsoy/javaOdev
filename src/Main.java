cbc29b78c6f2839b6848ccdd4d6d0e69d164028e
akinozsoy <akn.ozsy5757@gmail.com>
2022-09-20 14:12:03 +0300

    Java Ödev

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
+    //resuability
     public static void main(String[] args) {
 
         String merhaba = "Merhaba DÜNYA";
+        String mesaj = "Öğrenci Sayısı : ";
         System.out.println("Hello World");
         System.out.println("Merhaba Java");
         System.out.println(merhaba);
+
+        int ogrenciSayisi = 11;
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println("Öğrenci Sayım : " +ogrenciSayisi);
+        System.out.println("Öğrenci Sayım : " +ogrenciSayisi);
+        System.out.println("Öğrenci Sayım : " +ogrenciSayisi);
+        System.out.println("Öğrenci Sayım : " +ogrenciSayisi);
+        System.out.println("Öğrenci Sayım : " +ogrenciSayisi);
+
     }
+
 }
