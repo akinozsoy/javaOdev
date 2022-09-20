0af128f2aaf48c323612dea0e40f6d85bc1ceae4
akinozsoy <akn.ozsy5757@gmail.com>
2022-09-20 12:59:10 +0300

    Java Ödev

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String merhaba = "Merhaba DÜNYA";
+        System.out.println("Hello World");
+        System.out.println("Merhaba Java");
+        System.out.println(merhaba);
+    }
+}
