e86665a2a5ec5b5b616cde767dd816a1b584b695
akinozsoy <akn.ozsy5757@gmail.com>
2022-09-20 14:18:12 +0300

    Java Ödev

--- src/dataTypes.java
+++ src/dataTypes.java
@@ -1,5 +1,10 @@
 public class dataTypes {
      public static void main(String[]args){
-
+          int sayi = 12;
+          double ondalikliSayilar = 12.5;
+          byte degerKucuk = 127;
+          System.out.println(sayi);
+          System.out.println(ondalikliSayilar);
+          System.out.println(degerKucuk);
      }
 }
