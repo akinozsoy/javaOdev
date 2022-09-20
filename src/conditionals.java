e572167eb4d2db646238cf3096f7f6603c380c13
akinozsoy <akn.ozsy5757@gmail.com>
2022-09-20 20:16:06 +0300

    Java Ödev

---
+++ src/conditionals.java
@@ -0,0 +1,37 @@
+public class conditionals {
+    public static void main(String[] args){
+        int sayi = 10;
+        if (sayi < 20){
+            System.out.println("sayı 20 den küçüktür");
+        }
+        else if(sayi == 20){
+            System.out.println("sayı 20 ye eşittir");
+        }
+        else{
+            System.out.println("Sayı 20 den büyüktür");
+        }
+        int sayi2 = 20;
+        if (sayi2 < 20){
+            System.out.println("sayı 20 den küçüktür");
+        }
+        else if(sayi2 == 20){
+            System.out.println("sayı 20 ye eşittir");
+        }
+        else{
+            System.out.println("Sayı 20 den büyüktür");
+        }
+        int sayi3 = 10;
+        if (sayi3 < 20){
+            System.out.println("sayı 20 den küçüktür");
+        }
+        else if(sayi3 == 20){
+            System.out.println("sayı 20 ye eşittir");
+        }
+        else{
+            System.out.println("Sayı 20 den büyüktür");
+        }
+    }
+    }
+
+
+
