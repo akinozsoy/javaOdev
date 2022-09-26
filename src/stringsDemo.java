6a698617b8b9943392719dac23a65b591a1ee0df
Akın Özsoy <akn.ozsy5757@gmail.com>
2022-09-26 09:57:36 +0300

    Java Ödev

--- src/stringsDemo.java
+++ src/stringsDemo.java
@@ -1,10 +1,18 @@
 public class stringsDemo {
-    public static void main(String[]args){
-        String mesaj = "Bugün Hava Çok Güzel";
+    public static void main(String[] args) {
+        String mesaj = "Bugün Hava Çok Güzel ";
 
         System.out.println(mesaj);
 
-        System.out.println("Eleman Sayısı : "+mesaj.length());
-        System.out.println("5. Elemna");
+        System.out.println("Eleman Sayısı : " + mesaj.length());
+        System.out.println("5. Eleman : " + mesaj.length());
+        System.out.println(mesaj.concat("Yaşasın!")); // Yeni Bir String Değeri Oluşturuyor
+        System.out.println(mesaj);
+        System.out.println(mesaj.startsWith("B"));
+        System.out.println(mesaj.endsWith("."));
+        char[] karakterler = new char[5];
+        mesaj.getChars(0, 5,karakterler,0);
+        System.out.println(mesaj.indexOf("av"));
+        System.out.println(mesaj.lastIndexOf("e"));
     }
 }
