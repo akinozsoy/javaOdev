eef5cec22ce8668e1a35c409bd1da079b88423ad
Akın Özsoy <akn.ozsy5757@gmail.com>
2022-09-30 08:38:18 +0300

    Java Ödev

--- src/methods2.java
+++ src/methods2.java
@@ -6,6 +6,7 @@ public class methods2 {
         System.out.println(yeniMesaj);
         int sayi = topla(5,7);
         System.out.println(sayi);
+        int toplam = topla2(2,3,4,5,6,7,8,9,0);
 
     }
     public static void ekle (){
@@ -19,6 +20,14 @@ public class methods2 {
     }
     public static int topla(int sayi1,int sayi2){
        return sayi1 + sayi2;
+    }
+    public static int topla2(int... sayilar){
+
+        int toplam = 0;
+        for (int sayi: sayilar) {
+            toplam = toplam += sayi;
+        }
+        return toplam;
     }
     public static String sehirVer(){
 
