13f3a09b13cff7f4102710293f304e77f64ce42c
Akın Özsoy <akn.ozsy5757@gmail.com>
2022-10-03 11:58:12 +0300

    Java Ödev

--- src/Product.java
+++ src/Product.java
@@ -1,7 +1,11 @@
 public class Product {
+    //Attribute
     int id;
     String name;
     String description;
     double price;
     int stockAmount;
+    public  void Add(Product product){
+        System.out.println("Ürün Eklendi "+ product.name);
+    }
 }
